9da4735524a4ff915ee2904180a39bd8f2eec753
YuriiGreg <yuriyf888@gmail.com>
2024-07-22 15:16:43 +0200

    chengeImageModal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,29 +1,42 @@
+import React, { useEffect } from 'react';
 import ReactModal from 'react-modal';
-import PropTypes from 'prop-types';
 import styles from './ImageModal.module.css';
 
-ReactModal.setAppElement('#root');
+const ImageModal = ({ isOpen, onRequestClose, image }) => {
+  useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
+        onRequestClose();
+      }
+    };
 
-const ImageModal = ({ isOpen, onRequestClose, image }) => (
-  <ReactModal
-    isOpen={isOpen}
-    onRequestClose={onRequestClose}
-    className={styles.modal}
-    overlayClassName={styles.overlay}
-  >
-    <img src={image.urls.regular} alt={image.alt_description} className={styles.image} />
-  </ReactModal>
-);
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onRequestClose]);
 
-ImageModal.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onRequestClose: PropTypes.func.isRequired,
-  image: PropTypes.shape({
-    urls: PropTypes.shape({
-      regular: PropTypes.string.isRequired,
-    }).isRequired,
-    alt_description: PropTypes.string,
-  }).isRequired,
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className={styles.modalContent}
+      overlayClassName={styles.modalOverlay}
+    >
+      <div className={styles.modalBody} onClick={onRequestClose}>
+        <img
+          src={image.urls.regular}
+          alt={image.alt_description}
+          className={styles.modalImage}
+          onClick={e => e.stopPropagation()}
+        />
+        <button onClick={onRequestClose} className={styles.closeButton}>
+          Close
+        </button>
+      </div>
+    </ReactModal>
+  );
 };
 
 export default ImageModal;
+
